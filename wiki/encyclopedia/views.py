670b16288f7cd0b0cbf7336f0122c2263c29a2ee
Nabeel Fahim <nabeelfahim@icloud.com>
2024-01-26 22:20:00 +0530

    completing entry page

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import markdown2
 
 from . import util
 
@@ -7,4 +8,15 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def page_render(request, TITLE):
+    md_content = util.get_entry(TITLE)
+    if md_content is None:
+        return render(request, "encyclopedia/error.html")
+    html_content = markdown2.markdown(md_content)
+    return render(request, "encyclopedia/content.html" , {
+        "html_content" : html_content, "TITLE" : TITLE
+    })
+    
+
 
