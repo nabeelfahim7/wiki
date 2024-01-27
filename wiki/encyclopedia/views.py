8847467d6ac2e17e98b82df559eff97365139364
Nabeel Fahim <nabeelfahim@icloud.com>
2024-01-27 20:50:45 +0530

    completing search page

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render
 import markdown2
+from django import forms
+from django.http import HttpResponse
+
 
 from . import util
 
 
+
 def index(request):
+    if request.method == "POST":
+        sub_list = []
+        query = request.POST.get('q')
+        for entry in util.list_entries():
+            if query.upper() in entry.upper():
+                sub_list.append(entry)
+            if entry.upper() == str(query).upper():
+                return page_render(request, entry)
+        # create a new list that includes substrings 
+        return render(request, "encyclopedia/search.html", {
+        "entries": sub_list, "query" : query
+    })
+        
+        
+            
+        
+
+
+
+
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
